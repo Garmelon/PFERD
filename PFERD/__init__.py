9693e1d9680c86e2f7ea8e7fd91eebfbfdc8efe8
Joscha <joscha@plugh.de>
2019-04-25 19:41:26 +0000

    Make logging easier

--- PFERD/__init__.py
+++ PFERD/__init__.py
@@ -1,12 +1,35 @@
+import logging
+
 from .ffm import *
 from .ilias import *
 from .norbert import *
 from .utils import *
 
-__all__ = []
+__all__ = ["STYLE", "FORMAT", "DATE_FORMAT", "FORMATTER", "enable_logging"]
+
 __all__ += ffm.__all__
 __all__ += ilias.__all__
 __all__ += norbert.__all__
 __all__ += utils.__all__
 
-LOG_FORMAT = "[%(levelname)s] %(message)s"
+STYLE = "{"
+FORMAT = "[{levelname:<7}] {message}"
+DATE_FORMAT = "%F %T"
+
+FORMATTER = logging.Formatter(
+        fmt=FORMAT,
+        datefmt=DATE_FORMAT,
+        style=STYLE,
+)
+
+def enable_logging(name="PFERD", level=logging.INFO):
+    handler = logging.StreamHandler()
+    handler.setFormatter(FORMATTER)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    # This should be logged by our own handler, and not the root logger's
+    # default handler, so we don't pass it on to the root logger.
+    logger.propagate = False
