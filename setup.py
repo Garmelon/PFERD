59c278da2c7ce9245304af147ab1224bf39b0d80
Joscha <joscha@plugh.de>
2019-05-06 12:07:12 +0000

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name="PFERD",
-        version="1.1.0",
+        version="1.1.1",
         packages=["PFERD"],
         install_requires=[
             "requests>=2.21.0",
