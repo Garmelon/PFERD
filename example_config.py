3a57430893dfe31b10ead62e59df238ffa13467a
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-04-23 12:35:58 +0200

    Fix type errors in example_config

--- example_config.py
+++ example_config.py
@@ -3,11 +3,11 @@ from pathlib import Path
 from PFERD import Pferd, enable_logging
 
 
-def main():
+def main() -> None:
     enable_logging()
     pferd = Pferd(Path(__file__).parent)
 
-    pferd.ilias_kit("DB", "1101554", cookies="ilias_cookies.txt")
+    pferd.ilias_kit(Path("DB"), "1101554", cookies=Path("ilias_cookies.txt"))
 
 
 if __name__ == "__main__":
