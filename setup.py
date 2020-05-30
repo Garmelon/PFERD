f614b95a006fbb463187e674c4fbfcf73388f3d2
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-05-30 19:07:02 +0200

    Adjust version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="PFERD",
-    version="2.0.0",
+    version="2.1.0",
     packages=find_packages(),
     install_requires=[
         "requests>=2.21.0",
