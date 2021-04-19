c1ab7485e248c9bbcfa40405ba9a2dd1713784d1
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2021-04-19 11:21:56 +0200

    Bump version to 2.6.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="PFERD",
-    version="2.6.0",
+    version="2.6.1",
     packages=find_packages(),
     install_requires=[
         "requests>=2.21.0",
