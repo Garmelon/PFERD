5595a908d883f1f3da5a14790017085002e5d3e4
Joscha <joscha@plugh.de>
2021-04-27 00:29:42 +0200

    Configure entry point

--- PFERD/__init__.py
+++ PFERD/__init__.py
@@ -1,8 +1,2 @@
-# pylint: disable=invalid-name
-
-"""
-This module exports only what you need for a basic configuration. If you want a
-more complex configuration, you need to import the other submodules manually.
-"""
-
-from .pferd import Pferd
+def main() -> None:
+    print("Hello world")
