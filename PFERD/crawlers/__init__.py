595ba8b7ab601c90b930f36c4c63a194deac8fb8
Joscha <joscha@plugh.de>
2021-05-10 23:47:46 +0200

    Remove dummy crawler

--- PFERD/crawlers/__init__.py
+++ PFERD/crawlers/__init__.py
@@ -3,10 +3,8 @@ from typing import Callable, Dict
 
 from ..config import Config
 from ..crawler import Crawler, CrawlerSection
-from .dummy import DummyCrawler
 from .local import LocalCrawler, LocalCrawlerSection
 
 CRAWLERS: Dict[str, Callable[[str, Config, SectionProxy], Crawler]] = {
-    "dummy": lambda n, c, s: DummyCrawler(n, c, CrawlerSection(s)),
     "local": lambda n, c, s: LocalCrawler(n, c, LocalCrawlerSection(s)),
 }
