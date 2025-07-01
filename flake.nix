3755f593ff60e06ffbeab01135f924c1f3664453
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2025-07-01 14:33:11 +0200

    Update nix flake to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Tool for downloading course-related files from ILIAS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs }:
